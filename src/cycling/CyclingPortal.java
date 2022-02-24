5c8c6a28b672833974421ecf856390e0f897fa19
Joey <josefgriffiths@me.com>
2022-02-24 13:10:00 +0000

    Added TODO list

--- src/cycling/CyclingPortal.java
+++ src/cycling/CyclingPortal.java
@@ -1,5 +1,67 @@
 package cycling;
 
 public class CyclingPortal {
-    
+    /*
+    TODO Attributes for CyclingPortal class:
+            ???
+
+    TODO Create Riders class:
+    Variables:
+    riderID (int)
+        ???????????????????????????????????
+    stageResults (dictionary thing?)
+            ?adjustedElapsedTime (dictionary thing?)
+    ranks (dictionary thing?)
+            ?adjustedElapsedTime (dictionary thing?)
+    points (dictionary thing?)
+    mountainPoints (dictionary thing?)
+    raceResults (dictionary thing?)
+    points (dictionary thing?)
+    mountainPoints(dictionary thing?)
+    generalClassificationRank (dictionary thing?)
+    pointsClassificationRank (dictionary thing?)
+    mountainPointsClassificationRank (dictionary thing?)
+        ???????????????????????????????????
+
+    TODO Create Teams class:
+    Variables:
+    teamID (int)
+    teamName (str)
+    teamDescription (str)
+    teamRiders ([] or <>)
+
+
+    TODO Create Segment class:
+    Variables:
+    segmentID (int)
+    segmentLocation (double)
+    segmentType (SegmentType)
+    averageGradient (double)
+    segmentLength (double)
+
+    TODO Create Stages class:
+    Variables:
+    stageID (int)
+    stageName (str)
+    stageDescription (str)
+    stageLength (double)
+    stageStartTime (time?)
+    stageType (StageType)
+    segments ([] or <>)
+
+    TODO Create Races class:
+    Variables:
+    raceID (int)
+    raceName (str)
+    raceDescription (str)
+    noOfStages (int)
+    stages ([] or <>)
+    teams ([] or <>)
+    generalClassificationTimes (dictionary thing?)
+    Constructors:
+    Race(str name, str description):
+    Auto-gen raceID
+
+    TODO Implement functionality for each method in BadCyclingPortal
+ */
 }
