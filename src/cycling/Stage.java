c0f372d903c19e16173f0852fccafe0b1cb742be
Joey <josefgriffiths@me.com>
2022-02-24 13:47:04 +0000

    Added Stage class

---
+++ src/cycling/Stage.java
@@ -0,0 +1,19 @@
+package cycling;
+
+//TODO fill in document
+/**
+
+*/
+public class Stage {
+    /*
+    TODO Variables:
+    stageID (int)
+    stageName (str)
+    stageDescription (str)
+    stageLength (double)
+    stageStartTime (time?)
+    stageType (StageType)
+    segments ([] or <>)
+    */
+    private int stageID;
+}
