376e4154369589d8e48d0d4abb3117225edc9e68
Joey <josefgriffiths@me.com>
2022-03-02 10:49:31 +0000

    Implemented stageresults class

---
+++ src/cycling/StageResult.java
@@ -0,0 +1,17 @@
+// Stores the results of stage results
+
+public class StageResult {
+    private Stage stage;
+    private Rider rider;
+    private LocalTime time;
+
+    StageResult(Stage stage, Rider rider, LocalTime time) {
+        this.stage = stage;
+        this.rider = rider;
+        this.time = time;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}
