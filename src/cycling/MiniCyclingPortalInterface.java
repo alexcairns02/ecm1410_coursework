67b4877cb66eb73e8643509ce3de9d6ba40facfe
Joey <josefgriffiths@me.com>
2022-03-21 21:14:54 +0000

    Corrected Diogo spelling mistakes.

--- src/cycling/MiniCyclingPortalInterface.java
+++ src/cycling/MiniCyclingPortalInterface.java
@@ -404,7 +404,7 @@ public interface MiniCyclingPortalInterface extends Serializable {
 	void deleteRiderResultsInStage(int stageId, int riderId) throws IDNotRecognisedException;
 
 	/**
-	 * Get the riders finished position in a a stage.
+	 * Get the riders finished position in a stage.
 	 * <p>
 	 * The state of this MiniCyclingPortalInterface must be unchanged if any
 	 * exceptions are thrown.
@@ -440,7 +440,7 @@ public interface MiniCyclingPortalInterface extends Serializable {
 	 * exceptions are thrown.
 	 * 
 	 * @param stageId The ID of the stage being queried.
-	 * @return The ranked list of points each riders received in the stage, sorted
+	 * @return The ranked list of points each rider received in the stage, sorted
 	 *         by their elapsed time. An empty list if there is no result for the
 	 *         stage. These points should match the riders returned by
 	 *         {@link #getRidersRankInStage(int)}.
@@ -456,7 +456,7 @@ public interface MiniCyclingPortalInterface extends Serializable {
 	 * exceptions are thrown.
 	 * 
 	 * @param stageId The ID of the stage being queried.
-	 * @return The ranked list of mountain points each riders received in the stage,
+	 * @return The ranked list of mountain points each rider received in the stage,
 	 *         sorted by their finish time. An empty list if there is no result for
 	 *         the stage. These points should match the riders returned by
 	 *         {@link #getRidersRankInStage(int)}.
